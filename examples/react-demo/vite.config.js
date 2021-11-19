515d8b93e60d34b4069b0a1f3fc3c3ceb1bb3eda
ZxBing0066 <ZxBing0066@gmail.com>
2021-11-19 10:22:29 +0800

    update package config & demo build

--- examples/react-demo/vite.config.js
+++ examples/react-demo/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: path.join(__dirname, '../../docs/examples/react-demo')
-    }
+export default defineConfig(({ command, mode }) => {
+    return {
+        plugins: [react(), ViteEjsPlugin()],
+        build: {
+            outDir: path.join(__dirname, '../../docs/examples/react-demo')
+        }
+    };
 });
